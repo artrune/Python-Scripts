de32f58f637c621171d994ab93bf6336920b4854
Arturo Aguilar <artrune@hotmail.com>
2021-05-21 15:20:05 -0500

    Removes

--- influx 1.8 to json to csv/j2csv.py
+++ influx 1.8 to json to csv/j2csv.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-with open('temp.json', encoding='utf-8-sig') as f_input:
+with open('humidity.json', encoding='utf-8-sig') as f_input:
    json = pd.read_json(f_input)
 
 list = (([date, value]) for date, value in json['values'])
 
-df = pd.DataFrame(list, columns=["time","temp"])
-df.to_csv("temp.csv", index=False)
+df = pd.DataFrame(list, columns=["time","humidity"])
+df.to_csv("humidity.csv", index=False)
